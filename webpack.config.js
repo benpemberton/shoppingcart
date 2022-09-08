a1a66106bc99cdc905eda453583c10360fac088d
benpemberton <benedictpemberton@gmail.com>
2022-09-08 09:57:17 +0000

    ejected react to use webpack instead. pruned dependencies.

---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = () => {
+  let isDevelopment = process.env.NODE_ENV === "development";
+
+  return {
+    mode: isDevelopment ? "development" : "production",
+    entry: "./src/index.js",
+    devtool: isDevelopment ? "inline-source-map" : false,
+    devServer: {
+      port: 3010,
+      open: true,
+      allowedHosts: "all",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Shopping Cart",
+        template: "./src/template.html",
+      }),
+      new MiniCssExtractPlugin(),
+      // new Dotenv(),
+    ],
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(scss|css)$/,
+          include: path.resolve(__dirname, "src"),
+          sideEffects: true,
+          use: [
+            isDevelopment
+              ? "style-loader"
+              : {
+                  loader: MiniCssExtractPlugin.loader,
+                },
+            {
+              loader: "css-loader",
+              // options: {
+              //   modules: {
+              //     localIdentName: isDevelopment
+              //       ? "[name]__[local]"
+              //       : "[hash:base64:5]",
+              //     exportLocalsConvention: "dashes",
+              //   },
+              // },
+            },
+            // {
+            //   loader: "sass-loader",
+            //   options: {
+            //     sourceMap: isDevelopment,
+            //   },
+            // },
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              cacheDirectory: true,
+              cacheCompression: false,
+              presets: [
+                "@babel/preset-env",
+                [
+                  "@babel/preset-react",
+                  {
+                    runtime: "automatic",
+                  },
+                ],
+              ],
+            },
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".jsx", ".scss"],
+    },
+  };
+};
