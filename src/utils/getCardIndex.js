0c141b4fa11154899420c326aa842c3cc9e65a61
benpemberton <78801225+benpemberton@users.noreply.github.com>
2023-01-20 10:41:29 +0000

    added files to root

--- src/utils/getCardIndex.js
+++
@@ -1,6 +0,0 @@
-import store from "../redux/store";
-
-export const getCartIndex = (id) => {
-    const cartItems = store.getState().cart.cartItems;
-    return cartItems.findIndex((item) => item.id === id);
-}
